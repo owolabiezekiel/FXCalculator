4ebd56a2bc8de6b019c0b0052c0bd256cb56c867
=tobilobaezekiel <owo.ezekiel@gmail.com>
2017-09-19 10:55:34 +0100

    added calculator logic

--- src/fxcalculator/FXMLDocumentController.java
+++ src/fxcalculator/FXMLDocumentController.java
@@ -5,12 +5,15 @@
  */
 package fxcalculator;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,14 +21,74 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+    private BigDecimal left;
+    private String operator;
+    private boolean numberInput;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField viewArea;
+
+    public FXMLDocumentController() {
+        this.left = BigDecimal.ZERO;
+        this.operator = "";
+        this.numberInput = false;
     }
+    
+    @FXML
+    public void buttonClicked(ActionEvent e) {
+        Button button = (Button)e.getSource();
+        String buttonValue = button.getText();
+        
+        if(buttonValue.equals("C")){
+            if(buttonValue.equals("C")){
+                left = BigDecimal.ZERO;
+            }
+            operator = "";
+            numberInput = true;
+            viewArea.setText("0");
+            return;
+        }
+        
+        if(buttonValue.matches("[0-9\\,]")) {
+            if(!numberInput){
+                numberInput = true;
+                viewArea.clear();
+            }
+            viewArea.appendText(buttonValue);
+            return;
+        }
+        
+        if( buttonValue.matches("+-*/")) {  
+        } else { 
+            left = new BigDecimal(viewArea.getText());
+            operator = buttonValue;
+            numberInput = false;
+            return;
+        }
+        
+        if(buttonValue.equals("=")){
+            final BigDecimal right = numberInput ? new BigDecimal(viewArea.getText()) : left;
+            left = calculate(operator, left, right);
+            viewArea.setText(left.toString());
+            numberInput = false;
+            return;
+        }
+    }
+    
+    public static BigDecimal calculate(String operator, BigDecimal left, BigDecimal right){
+        switch(operator) {
+            case "+" :
+                return left.add(right);
+            case"-":
+                return left.subtract(right);
+            case "*" :
+                return left.multiply(right);
+            case"/":
+                return left.divide(right);
+        }
+        return right;
+    }
+   
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
